0aeadbb9f2f9e1e13bc158c4fa5d0308a0b31c9f
chris373 <cdcauseydev@gmail.com>
2016-10-12 11:14:04 -0400

    Only one post is now shown per page. Implemented links from the archive back to the post.

--- build.py
+++ build.py
@@ -8,93 +8,120 @@ import json
 import time
 import copy
 
-class json_handler:
-    def load(self, filename):
-        with open(filename, 'r') as json_data:
-            return json.load(json_data)
-    def write(self, filename, json_data):
-        with open(filename, 'w') as f:
-            json.dump(json_data, f, indent=4)
+def cast(value, cast_type):
+    if cast_type == 'int':
+        return int(value)
+    if cast_type == 'float':
+        return float(value)
+    if cast_type == 'string':
+        return str(value)
 
 
-class archive:
-    def __init__(self, filename, json_handler):
-        self.json_data = json_handler.load(filename)
-    def update(self, element_dict):
-        self.json_data.update(element_dict)
+def get_extension(file):
+    return file.split('.')[-1]
 
 
-class jinja_templater:
-    def __init__(self, config):
-        self.env = Environment(loader=PackageLoader(config['site_path'], config['templates_path']))
-    def apply(self, apply_template, output, **kwargs):
-        template = self.env.get_template(apply_template)
-        with open(output, 'w') as f:
-            f.write(template.render(kwargs))
+def remove_extensions(file):
+    return '.'.join(file.split('.')[:-1])
 
 
-def paginate(config, posts, sort_key, per_page_count):
-    # sort, then reverse for chronological order
-    # change each post to a tuple of (post[sort_key], post)
-    posts = [(posts[post][sort_key], posts[post]) for post in posts]
-    posts = sorted(posts, key=lambda post: float(post[0]))[::-1]
-    # change it back to just the post
-    posts = [post[1] for post in posts]
-    pages = [[] for i in range(0, math.ceil( len(posts) / float(per_page_count) ))]
-    for i in range(0, len(posts)):
-        pages[math.floor(i / int(per_page_count))].append(posts[i])
-    return pages
+def write_changes(json_data):
+    json_data_str = json.dumps(json_data)
+    json_config_str = json.dumps(json_data['config'])
+    with open('config.json', 'w') as f:
+        json.dump(json_data['config'], f, indent=4)
+    json_data.pop('config')
+    with open('archive.json', 'w') as f:
+        json.dump(json_data, f, indent=4)
+
+def get_config(json_data):
+    config = json_data['config']
+    cdict = dict()
+    for con in config:
+        contype = con['type']
+        keys = [key for key in con]
+        # remove type, leaving only the config value
+        keys.remove('type')
+        cdict[keys[0]] = cast(con[keys[0]], contype)
+    return cdict
 
 
-def load_content(filename):
-    with open(filename, 'r') as f:
-        content = f.read()
-    return content
+def load_json():
+    with open('archive.json', 'r') as archive:
+        json_data = json.load(archive)
+    with open('config.json', 'r') as config:
+        json_data['config'] = json.load(config)
+    return json_data
 
 
-def build_site(config, archive, templater):
-    # build archive page(s)
-    archive_pages = paginate(config, archive, 'mtime', config['archive_pagination_count'])
-    for i, page in enumerate(archive_pages):
-        buttons = get_buttons(i, len([k for k in archive]), config['archive_pagination_count'], config['pagination_button_count'])
-        save_to = os.path.join(config['site_path'], 'archive', str(i) + '.html')
-        templater.apply(config['archive_template'], save_to, posts=page, buttons=buttons)
-
-    # build blog page(s)
-    blog_pages = paginate(config, archive, 'mtime', config['blog_pagination_count'])
-    # each post in blog_pages does not contain the posts content, load it in.
-    for page in blog_pages:
-        for post in page:
-            post['content'] = load_content(os.path.join(config['posts_path'], post['filename']))
-    for i, page in enumerate(blog_pages):
-        buttons = get_buttons(i, len([k for k in archive]), config['blog_pagination_count'], config['pagination_button_count'])
-        save_to = os.path.join(config['site_path'], 'blog', str(i) + '.html')
-        templater.apply(config['blog_template'], save_to, posts=page, buttons=buttons)
+def gen_post_entry(files_directory, post_file, count, config):
+    entrydict = dict()
+    entrydict['title'] = post_file
+    t = os.path.getmtime(files_directory + '/' + post_file)
+    gmt = time.gmtime(t)
+    entrydict['publish_date'] = time.strftime(config['date_format_string'], gmt)
+    entrydict['ID'] = count
+    entrydict['FNAME'] = post_file
+    return entrydict
 
 
-def update_archive(config, archive):
+def clear_site(config):
+    # remove the old verison of the site and recreate the folder
+    shutil.rmtree(config['site_path'], ignore_errors=True)
+    os.makedirs(config['site_path'], exist_ok=False)
+
+
+def update_archive(config, json_data):
+    # look for posts in config['post_path']
     # if it's not in the archive, add it
+    archive_posts = json_data['posts']
     # os.walk yields a 3-tuple (dirpath, dirnames, filenames)
     step = next(os.walk(config['posts_path']))
     files_directory = step[0]
     files = step[2]
-    for f in files:
-        if f not in archive:
-            archive[f] = {}
-            fdict = archive[f]
-            t = os.path.getmtime(files_directory + '/' + f)
-            fdict['mtime'] = str(t)
-            gmt = time.gmtime(t)
-            fdict['publish_date'] = time.strftime(config['date_format_string'], gmt)
-            fdict['title'] = '.'.join(f.split('.')[:-1])
-            fdict['filename'] = f
+
+    # make sure all filenames are unique
+    assert len(files) == len(set(files))
+
+    count = len(archive_posts) # this becomes the added posts ID
+
+    # build a dictionary of posts in archive where FNAME is key
+    archive_postsdict = dict()
+    for post in archive_posts:
+        FNAME = post['FNAME']
+        archive_postsdict[FNAME] = post
+
+    # add posts not in the archive_posts
+    posts_to_add = []
+    for post_file in files:
+        if post_file not in archive_postsdict:
+            posts_to_add.append(post_file)
+
+    # order by modification date
+    # create a tuple of (m date, add_post)
+    for i, add_post in enumerate(posts_to_add):
+        mtime = os.path.getmtime(config['posts_path'] + '/' + add_post)
+        posts_to_add[i] = (mtime, add_post)
+    posts_to_add = sorted(posts_to_add, key=lambda add_post: add_post[0])
+    # posts need to be displayed in reverse chronological order so reverse them.
+    posts_to_add = reversed(posts_to_add)
+
+    # generate post entrys for the posts being added to the archive
+    for post_file in posts_to_add:
+        post_file = post_file[1] # get just the file name from the tuple
+        archive_posts.append(gen_post_entry(files_directory, post_file, count, config))
+        count += 1
+
+    # update the json_data with the updated list of posts
+    json_data['posts'] = archive_posts
+
+def load_content(config, post_fname):
+    with open(config['posts_path'] + '/' + post_fname, 'r') as f:
+        content = f.read()
+    return content
 
 
 def get_buttons(cur_page, post_count, pagination_count, button_count):
-    post_count = int(post_count)
-    pagination_count = int(pagination_count)
-    button_count = int(button_count)
-
     page_count = int(math.ceil(post_count / float(pagination_count)))
     halfb = int(button_count/2)
     lhalf = page_count - halfb
@@ -142,32 +169,66 @@ def get_buttons(cur_page, post_count, pagination_count, button_count):
     button_dicts.append({'active': last_forward_active, 'link': last_link })
     return button_dicts
 
+def build_archive_page(config, json_data):
 
-def file_setup(config):
-    # remove the old verison of the site and recreate the folder
-    shutil.rmtree(config['site_path'], ignore_errors=True)
-    os.makedirs(config['site_path'], exist_ok=False)
+    env = Environment(loader=PackageLoader(
+                        config['site_path'], config['templates_path']))
+    template = env.get_template(config['archive_template'])
+
+    # build so many posts at a time based on pagination_count
+    posts = copy.deepcopy(json_data['posts'])
+    count = len(posts)
+    pcount = config['archive_pagination_count'] # use a shorter name for readability
+    blog_page_count = int(math.ceil(len(posts) / float(pcount)))
+    for i in range(0, blog_page_count):
+        page_posts = posts[(i * pcount): (i * pcount) + pcount]
+        for j, post in enumerate(page_posts):
+            post['link'] = '../blog/' + str(j + (i*pcount)) + '.html'
+        buttons = get_buttons(i, count, config['archive_pagination_count'], config['archive_pagination_button_count'])
+        # render the current page
+        with open(config['site_path'] + '/archive/' + str(i) + '.html', 'w') as f:
+            f.write(template.render(posts=page_posts, buttons=buttons,))
+
+
+def build_site(config, json_data):
     # move static files over
     copy_tree('static', config['site_path'])
     shutil.copytree('css', config['site_path'] + '/css')
     shutil.copytree(config['templates_path'], config['site_path'] + '/templates')
-  # use indent of 4 for readability
+
     # create a folder to hold blog posts
     os.makedirs(config['site_path'] + '/blog')
     # create a folder to hold blog posts
     os.makedirs(config['site_path'] + '/archive')
-    with open(os.path.join(config['site_path'], '__init__.py'), 'w') as f:
-        f.write('')
+
+    # create an __init__.py file so that jinja2 recognizes site_path as a module
+    with open(config['site_path'] + '/__init__.py', 'w') as f:
+        pass
+
+    env = Environment(loader=PackageLoader(
+                        config['site_path'], config['templates_path']))
+    template = env.get_template(config['blog_template'])
+
+    # build so many posts at a time based on pagination_count
+    posts = copy.deepcopy(json_data['posts'])
+    count = len(posts)
+    for i in range(0, count):
+        post = posts[i]
+        post['content'] = load_content(config, post['FNAME'])
+        # only show one post per page, so pass 1 as pagination_count
+        buttons = get_buttons(i, count, 1, config['pagination_button_count'])
+        # render the current page
+        with open(config['site_path'] + '/blog/' + str(i) + '.html', 'w') as f:
+            f.write(template.render(post=post, buttons=buttons))
 
 
 def build():
-    jsonhandler = json_handler()
-    config = jsonhandler.load('config.json')
-    archive = jsonhandler.load('archive.json')
-    file_setup(config)
-    templater = jinja_templater(config)
-    update_archive(config, archive)
-    jsonhandler.write('archive.json', archive)
-    build_site(config, archive, templater)
+    json_data = load_json()
+    config = get_config(json_data)
+    clear_site(config)
+    update_archive(config, json_data)
+    build_site(config, json_data)
+    build_archive_page(config, json_data)
+    write_changes(json_data)
 
 build()
